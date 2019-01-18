f1f55dcf6b394ed34ec11112af462e0081d189d8
DSauthier <danielsauthier@live.com>
2019-01-18 17:31:32 -0500

    graphql 101

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+// 1
+let idCount = links.length
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
+  // update: (parent,args)=>{
+  //   const link = {
+  //     id: parent.id,
+  //     description: args.description,
+  //     url: args.description
+  //   }
+
+  // }
+}
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+})
+
+server.start(() => console.log("serve is running on localhost:4000"))
